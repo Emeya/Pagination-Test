43028ff25485920ce15909d97c30eb3705b8c174
Manuel Soberanis <manuel.soberanis@dacodes.com.mx>
2021-06-17 12:57:31 -0500

    Initial Commit

---
+++ Pagination Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pagination Test
+//
+//  Created by Manuel Soberanis on 17/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pagination Test/Pagination_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pagination_TestApp.swift
+//  Pagination Test
+//
+//  Created by Manuel Soberanis on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Pagination_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
